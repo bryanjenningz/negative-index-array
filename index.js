d630570260b0d22f9649f62d1b9129142a6dda98
Bryan Jennings <bryanjenningz@gmail.com>
2017-03-05 01:50:17 -0800

    add tests that check that errors are thrown

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const NegativeIndexArray = (startingArray = []) => (
       if (Number.isNaN(index)) {
         return target[name]
       } else {
-        if (Math.abs(index) > target.length) {
+        if (index < -target.length || index >= target.length) {
           throw new Error(`Out of range: ${index} for array length: ${target.length}`)
         }
         if (index < 0) {
@@ -30,8 +30,19 @@ const NegativeIndexArray = (startingArray = []) => (
   })
 )
 
+const throwsError = func => {
+  try {
+    func()
+  } catch (error) {
+    return true
+  }
+  return false
+}
+
 const xs = NegativeIndexArray([{a: 1}, {a: 2}, '23', 45])
 console.assert(xs[-1] === 45, 'last value is 45')
 console.assert(xs[-2] === '23', 'second last value is "23"')
 console.assert(xs[0] === xs[-4], 'the fourth last value is the zero index value')
 console.assert(xs[1] === xs[-3], 'the third last value is the one index value')
+console.assert(throwsError(() => xs[4]), 'throws error when accessing index 4 of length 4 array')
+console.assert(throwsError(() => xs[-5]), 'throws error when accessing index -5 of length 4 array')
